72d20090c64770d57ab936cb9f5d51d6c592dff5
Guillaume <guillaume@wechantloup.com>
2022-10-06 13:58:10 +0200

    Upload arduino code

---
+++ ms_converter/ms_converter.ino
@@ -0,0 +1,96 @@
+#include <Sleep_n0m1.h>
+
+#define RD 1
+#define CE 0
+#define D1 2
+#define D2 3
+#define D3 4
+#define D4 5
+#define D5 6
+#define D6 7
+#define D7 8
+
+Sleep sleep;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RD, INPUT_PULLUP);
+  pinMode(CE, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(CE, HIGH);
+  
+  outval(0x12); // DEC H
+  waitRdHigh();
+
+  outval(0x12); // DEC H
+  rdSync();
+
+  /* LD HL, $E101 */
+  outval(0x10);
+  rdSync();
+  outval(0x00);
+  rdSync();
+  outval(0x70);
+  rdSync();
+
+  /* DEC H */
+  outval(0x12);
+  rdSync();
+
+  /* LD SP,HL */
+  outval(0x7C);
+  rdSync();
+
+  /* RST $00 */
+  outval(0x63);
+  rdSync();
+
+  pinMode(CE, INPUT);
+  pinMode(D1, INPUT);
+  pinMode(D2, INPUT);
+  pinMode(D3, INPUT);
+  pinMode(D4, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
+  pinMode(D7, INPUT);
+  
+  sleep.pwrDownMode(); //set sleep mode
+  while (1) {
+    sleep.sleepDelay(0xffffffffL); //sleep for: max long, about 50 days
+  }
+}
+
+void waitRdLow() {
+  do {}
+  while (digitalRead(RD) == HIGH);
+}
+
+void waitRdHigh() {
+  do {}
+  while (digitalRead(RD) == LOW);
+}
+
+void rdSync() {
+  waitRdLow();
+  waitRdHigh();
+}
+
+void outval(byte value) {
+  digitalWrite(D1, value & 0x01 > 0);
+  digitalWrite(D2, value & 0x02 > 0);
+  digitalWrite(D3, value & 0x04 > 0);
+  digitalWrite(D4, value & 0x08 > 0);
+  digitalWrite(D5, value & 0x10 > 0);
+  digitalWrite(D6, value & 0x20 > 0);
+  digitalWrite(D7, value & 0x40 > 0);
+}
