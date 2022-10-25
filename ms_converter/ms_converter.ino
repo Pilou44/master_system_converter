79783637ef72414f0e82c152a01c9657e1cce86a
Guillaume <guillaume@wechantloup.com>
2022-10-25 09:28:54 +0200

    Code working with V1.0

--- ms_converter/ms_converter.ino
+++ ms_converter/ms_converter.ino
@@ -1,5 +1,9 @@
 #include <Sleep_n0m1.h>
 
+// PORTB = D8 to D13
+// PORTC = A0 to A7
+// PORTD = D0 to D7
+
 #define RD 12 // Rename OE
 #define CE 11
 #define D1 2
@@ -13,61 +17,85 @@
 
 Sleep sleep;
 
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(RD, INPUT_PULLUP);
-  pinMode(CE, OUTPUT);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
-  pinMode(D5, OUTPUT);
-  pinMode(D6, OUTPUT);
-  pinMode(D7, OUTPUT);
-
-  pinMode(LED, OUTPUT);
+//  pinMode(RD, INPUT_PULLUP);
+//  pinMode(CE, OUTPUT);
+//  pinMode(D1, OUTPUT);
+//  pinMode(D2, OUTPUT);
+//  pinMode(D3, OUTPUT);
+//  pinMode(D4, OUTPUT);
+//  pinMode(D5, OUTPUT);
+//  pinMode(D6, OUTPUT);
+//  pinMode(D7, OUTPUT);
+//
+//  pinMode(LED, OUTPUT);
+  DDRB = B00101001;
+  DDRD = 0xFC;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(CE, HIGH);
-  
-  outval(0x12); // DEC H
+//  digitalWrite(CE, HIGH);
+  PORTB = B00101000; // Force CE high on the cart $$ LED on
+
+//  outval(0x12); // DEC H
+// PORTB = CE & RD pullup = B00011000, PORTD = 0x12 << 2 = 0x48
+  outval(0x48, B00111000); // DEC H
   waitRdHigh();
 
-  outval(0x12); // DEC H
+  digitalWrite(LED, 1);
+
+//  outval(0x12); // DEC H
+// PORTB = CE & RD pullup = B00011000, PORTD = 0x12 << 2 = 0x48
+  outval(0x48, B00111000); // DEC H
   rdSync();
 
   /* LD HL, $E101 */
-  outval(0x10);
+//  outval(0x10);
+// PORTB = CE & RD pullup = B00011000, PORTD = 0x10 << 2 = 0x40
+  outval(0x40, B00111000); // DEC H
   rdSync();
-  outval(0x00);
+//  outval(0x00);
+// PORTB = CE & RD pullup = B00011000, PORTD = 0x00 << 2 = 0x00
+  outval(0x00, B00111000); // DEC H
   rdSync();
-  outval(0x70);
+//  outval(0x70);
+// PORTB = CE & RD pullup & 0x01 = B00011001, PORTD = 0x70 << 2 = 0xC0
+  outval(0xC0, B00111001); // DEC H
   rdSync();
 
   /* DEC H */
-  outval(0x12);
+//  outval(0x12);
+// PORTB = CE & RD pullup = B00011000, PORTD = 0x12 << 2 = 0x48
+  outval(0x48, B00111000); // DEC H
   rdSync();
 
   /* LD SP,HL */
-  outval(0x7C);
+//  outval(0x7C);
+// PORTB = CE & RD pullup & 0x01 = B00011001, PORTD = 0x7C << 2 = 0xF0
+  outval(0xF0, B00111001); // DEC H
   rdSync();
 
   /* RST $00 */
-  outval(0x63);
+//  outval(0x63);
+// PORTB = CE & RD pullup & 0x01 = B00011001, PORTD = 0x63 << 2 = 0x8C && LED OFF
+  outval(0x8C, B00011001); // DEC H
   rdSync();
 
-  pinMode(CE, INPUT);
-  pinMode(D1, INPUT);
-  pinMode(D2, INPUT);
-  pinMode(D3, INPUT);
-  pinMode(D4, INPUT);
-  pinMode(D5, INPUT);
-  pinMode(D6, INPUT);
-  pinMode(D7, INPUT);
+//  pinMode(CE, INPUT);
+//  pinMode(D1, INPUT);
+//  pinMode(D2, INPUT);
+//  pinMode(D3, INPUT);
+//  pinMode(D4, INPUT);
+//  pinMode(D5, INPUT);
+//  pinMode(D6, INPUT);
+//  pinMode(D7, INPUT);
+   DDRD = 0;
+   DDRB = 0;
   
-  digitalWrite(LED, 1);
+  digitalWrite(LED, 0);
   
 //  sleep.pwrDownMode(); //set sleep mode
   while (1) {
@@ -77,12 +105,14 @@ void loop() {
 
 void waitRdLow() {
   do {}
-  while (digitalRead(RD) == HIGH);
+//  while (digitalRead(RD) == HIGH);
+  while (PINB & 0x10);
 }
 
 void waitRdHigh() {
   do {}
-  while (digitalRead(RD) == LOW);
+//  while (digitalRead(RD) == LOW);
+  while (!PINB & 0x10);
 }
 
 void rdSync() {
@@ -98,4 +128,9 @@ void outval(byte value) {
   digitalWrite(D5, (value & 0x10) > 0);
   digitalWrite(D6, (value & 0x20) > 0);
   digitalWrite(D7, (value & 0x40) > 0);
+}
+
+void outval(byte portD, byte portB) {
+  PORTB = portB;
+  PORTD = portD;
 }
